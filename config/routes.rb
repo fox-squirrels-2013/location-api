3eed82b9132ce48681da8458b25848a8aa77ba0d
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-30 14:07:10 -0800

    Added routing for discounts api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 LocationApi::Application.routes.draw do
+  namespace :api do
+    name :v1 do
+      resources :discounts
+    end
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
