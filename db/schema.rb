7eaf02728028fa48339c2dfc6b16f2ff8c7bb6a4
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-30 14:29:07 -0800

    Added user models. All tests pass.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131130190306) do
+ActiveRecord::Schema.define(:version => 20131130222548) do
 
   create_table "discounts", :force => true do |t|
     t.integer  "price"
     t.integer  "percentage"
     t.string   "item"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
