d46ff937cdbe9b99dbb5a10f9d6970168f89496e
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-30 21:53:28 -0800

    Changed routes so everything in proper namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 LocationApi::Application.routes.draw do
   root to: 'discounts#index'
-
-  # will probably need to pass :locations through :stores resources
-  # this is because it belongs to :stores, and it needs to
-  # be able to call location.stores on it
-  resources :locations do
-    # :discounts belongs to locations, so it has to go through :locations
-    resources :discounts
+  resources :stores do
+    resources :locations do
+      resources :discounts
+    end
   end
 end
