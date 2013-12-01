1841b43e429fd0a35bcd487d71117bb1dbdbd624
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-30 21:34:50 -0800

    Made pending tests pass.

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Discount do
   it { should belong_to(:location) }
-  xit { should belong_to(:store) }
+  it { should belong_to(:store) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:item) }
   it { should validate_presence_of(:percentage) }
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Location do
   it { should have_many(:discounts) }
-  xit { should belong_to(:store) }
+  it { should belong_to(:store) }
   it { should validate_presence_of(:latitude) }
   it { should validate_presence_of(:longitude) }
   it { should validate_presence_of(:address) }
