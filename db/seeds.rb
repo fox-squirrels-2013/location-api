92572e49b24eec964e17e1c4b1f4119eac4e9d71
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-12-02 22:25:31 -0800

    Added a little seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 store = Store.create({name: 'Walgreens'})
 
-locations = Location.create([{store: store, latitude: '37.7923', longitude: '-122.4024', address: '300 Montgomery St, San Francisco, CA'}, {store: store, latitude: '37.7912', longitude: '-122.4007', address: '100 Sansome Street, San Francisco, CA'}, {store: store, latitude: '37.8796', longitude: '-122.2965', address: '1050 Gilman Street Berkeley, CA'}])
+locations = Location.create([{store: store, latitude: '37.7923', longitude: '-122.4024', address: '300 Montgomery St, San Francisco, CA'}, 
+                             {store: store, latitude: '37.7912', longitude: '-122.4007', address: '100 Sansome St, San Francisco, CA'}, 
+                             {store: store, latitude: '37.8796', longitude: '-122.2965', address: '1050 Gilman St Berkeley, CA'}])
 
-discounts = Discount.create([{store: store, location: locations.first, item: 'PBR', price: 10, percentage: 50}])
+discounts = Discount.create([{store: store, location: locations.first, item: 'PBR', price: 10, percentage: 50}, 
+                             {store: store, location: locations[1], item: 'Coors', price: 15, percentage: 40},
+                             {store: store, location: locations[2], item: 'Budweiser', price: 12, percentage: 60}])
